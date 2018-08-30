40eeeed2923a718ffe3e7688e96e00d8ef2f5151
Sai Kumar Mutoopuri <smutoopuri@InComm.com>
2018-08-29 22:15:32 -0400

    Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `<div class="container">
+  <h1>{{appTitle}}</h1>
               <h1>This is main Component</h1>
-            </div>`
+              <strong>{{title}}</strong>
+<app-prod></app-prod>
+<app-dash></app-dash></div>
+`
 
 })
 export class AppComponent {
   title = 'moduleApp';
+  appTitle = 'Welcome';
 }
